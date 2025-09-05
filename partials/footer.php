d7103bff34cddb4f38027493be1e79f9bab3eedf
Notropicalfish <130704104+Notropicalfish@users.noreply.github.com>
2025-09-04 22:11:19 -0400

    Create footer.php

---
+++ partials/footer.php
@@ -0,0 +1,11 @@
+    </main> <!-- Closes the .container started in the header -->
+
+    <footer class="site-footer">
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> MyPaper by Peelish Studios. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+</body>
+</html>
+
